167416e15d46eb3c19dad8b285acff683d93c065
Matt Simpson <matt.rc.simpson@gmail.com>
2023-01-15 18:54:01 +0200

    update package.json

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  useParams,
  } from "react-router-dom";
 import BreweryCard from "./components/BreweryCard";
 import BreweryInfo from "./components/BreweryInfo";
